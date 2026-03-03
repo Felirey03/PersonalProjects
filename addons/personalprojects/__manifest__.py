04506f6c6ca5804177dd18630eb4cb94d214f9b2
felipe <felirreynoso5@gmail.com>
2026-03-03 19:58:26 -0300

    [ADD] Creamos nuevo modulo

---
+++ addons/personalprojects/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Personal Projects',
+    'summary': 'Maneja tus proyectos personales',
+    'description': 'Una aplicación para gestionar tus proyectos personales, tareas y progreso.',
+    'license': 'AGPL-3',
+    'author': 'Felipe',
+    'website': 'https://github.com/Felirey03',
+    'category': 'Productividad',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+
+    'application': True,
+
+}
