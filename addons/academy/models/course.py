d8bf7a8a99759465ffc6375eddaf4dde8f01c308
felipe <felirreynoso5@gmail.com>
2026-03-20 13:43:11 -0300

    Agregamos vistas nuevas en session, y creamos primeros archivos para el modulo de ventas de los cursos

--- addons/academy/models/course.py
+++ addons/academy/models/course.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -14,4 +15,21 @@ class Course(models.Model):
                                  ('intermediate', 'Intermediate'),
                                  ('advanced', 'Advanced'),
                              ], copy=False)
+    session_ids = fields.One2many(comodel_name='academy.session', string='Sessions', inverse_name='course_id')
+    currency_id = fields.Many2one(comodel_name='res.currency', string='Currency'
+                                  , default=lambda self: self.env.company.currency_id.id)
+
+                                  
+    base_price = fields.Monetary(string='Price', currency_field='currency_id')
+    additional_fee = fields.Monetary(string='Additional Fee', currency_field='currency_id')
+    total_price = fields.Monetary(string='Total Price', currency_field='currency_id', compute='_compute_total_price')
+
+
+    @api.depends('base_price', 'additional_fee')
+    def _compute_total_price(self):
+        for records in self:
+            if(records.base_price < 0):
+                raise ValidationError("Base price cannot be negative.")
+            records.total_price = records.base_price + records.additional_fee
+
 
