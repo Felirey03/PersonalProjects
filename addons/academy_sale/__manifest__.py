8610e7d387bd7410ab7d4ea8a3c867f1d4a63dab
felipe <felirreynoso5@gmail.com>
2026-03-25 10:55:04 -0300

    Creamos nuevo modelo para el modulo de ventas del curso, demas de generar vistas necesarias para las compras de los mismos, con su propia categoria

---
+++ addons/academy_sale/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'academy_sale',
+    'author': 'Felipe',
+    'website': 'https://github.com/Felirey03/PersonalProjects',
+    
+    'category': 'Custom modules/Tech trainging',
+    'version': '1.0',
+    'licence': 'OPL-1',
+
+    'depends': ['academy', 'sale_management'],
+
+    'data': [
+        'data/academy_sale_data.xml',
+        'views/academy_sale_menuitems.xml',
+        'views/product_views.xml',
+        'views/course_views',
+        'views/product_template_views.xml',
+    ],
+    
+    'demo': [
+
+    ],
+
+    'auto_install': False,
+}
---
+++ addons/academy_sale/models/product_template.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    detailed_type = fields.Selection(selection_add=[('course', 'Course')], ondelete={'course': 'set service'})
+    course_ids = fields.Many2one(comodel_name="academy.course", inverse_name="product_id")
+
+    def _detailed_type_mapping(self):
+        type_mapping = super()._detailed_type_mapping()
+        type_mapping['course'] = 'service'
+        return type_mapping
